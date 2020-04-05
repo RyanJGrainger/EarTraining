ec83a69dbeefb0aca9c662c438296e327f5e8826
Ryan <ryangrainger2013@hotmail.co.uk>
2020-04-05 22:37:31 +0100

    fixed .play() issue

--- src/Pages/ChordRecognition.jsx
+++ src/Pages/ChordRecognition.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import AnswersButtons from "../components/AnswersButtons";
+import Chords from "../Chords"
 
 
 function playSound(){
-    var x = new Audio(".../public/sounds/pianoNotes/note10.mp3")
-    x.play();
-}
+    var note1 = new Audio("../sounds/pianoNotes/note10.mp3")
+    var note2 = new Audio("../sounds/pianoNotes/note15.mp3")
+    var note3 = new Audio("../sounds/pianoNotes/note17.mp3")
+    var note4 = new Audio("../sounds/pianoNotes/note21.mp3")
 
+    note1.play();
+    note2.play();
+    note3.play();
+    note4.play();
+
+    Chords.pop();
+}
 
 function ChordRecognition(){
     return (
-
-                    <div id="chordRecognition">
-                        <img onClick={playSound} className="tempPlayButton" src="../css/play-button.svg" alt="playbutton"/>
-                        <AnswersButtons />
-                    </div>
-        
+        <div id="chordRecognition">
+            <img onClick={playSound} className="tempPlayButton" src="../css/play-button.svg" alt="playbutton"/>
+            <AnswersButtons />
+        </div>
     );
-
-
 }
 
 export default ChordRecognition;

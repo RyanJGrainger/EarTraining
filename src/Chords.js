f42fb9b1c62eaae1065a24a109e94220f15a05dc
Ryan <ryangrainger2013@hotmail.co.uk>
2020-04-06 16:17:54 +0100

    plays random chords with no clipping

--- src/Chords.js
+++ src/Chords.js
@@ -1,16 +1,16 @@
 var Chords = [
-    {chordName:"Maj",chordID: "MajorButton"},
-    {chordName:"Min",chordID:"MinorButton"},
-    {chordName:"Sus 2",chordID:"Sus2Button"},
-    {chordName:"Sus 4",chordID:"Sus4Button"},
-    {chordName:"Maj 7",chordID:"Major7Button"},
-    {chordName:"Min 7",chordID:"Minor7Button"},
-    {chordName:"Dim",chordID:"DiminishedButton"},
-    {chordName:"Aug",chordID:"Augmented Button"},
-    {chordName:"Maj 6",chordID:"Major6Button"},
-    {chordName:"Min 6",chordID:"Minor6Button"},
-    {chordName:"Dom 7",chordID:"Dominant7Button"},
-    {chordName:"add 9",chordID:"Add9Button"},
+    {chordName:"Maj",chordID: "MajorButton",intervals:[4, 7]},
+    {chordName:"Min",chordID:"MinorButton",intervals:[3, 7]},
+    {chordName:"Sus 2",chordID:"Sus2Button",intervals:[2, 7]},
+    {chordName:"Sus 4",chordID:"Sus4Button",intervals:[5, 7]},
+    {chordName:"Maj 7",chordID:"Major7Button",intervals:[4, 7, 11]},
+    {chordName:"Min 7",chordID:"Minor7Button",intervals:[3, 7, 10]},
+    {chordName:"Dim",chordID:"DiminishedButton",intervals:[3, 6]},
+    {chordName:"Aug",chordID:"Augmented Button",intervals:[4, 8]},
+    {chordName:"Maj 6",chordID:"Major6Button",intervals:[4, 7, 9]},
+    {chordName:"Min 6",chordID:"Minor6Button",intervals:[3, 7, 9]},
+    {chordName:"Dom 7",chordID:"Dominant7Button",intervals:[4, 7, 10]},
+    {chordName:"add 9",chordID:"Add9Button",intervals:[2, 4, 7]},
 ];
 
 
--- src/Pages/ChordRecognition.jsx
+++ src/Pages/ChordRecognition.jsx
@@ -1,26 +1,53 @@
 import React from "react";
+import {Howl, Howler} from 'howler';
 import AnswersButtons from "../components/AnswersButtons";
 import Chords from "../Chords"
 
 
-function playSound(){
-    var note1 = new Audio("../sounds/pianoNotes/note10.mp3")
-    var note2 = new Audio("../sounds/pianoNotes/note15.mp3")
-    var note3 = new Audio("../sounds/pianoNotes/note17.mp3")
-    var note4 = new Audio("../sounds/pianoNotes/note21.mp3")
+function playChord(){
+    var root = Math.floor(Math.random() * 10) + 1;
+    var whichChord = Math.floor(Math.random() * 10);
 
-    note1.play();
-    note2.play();
-    note3.play();
-    note4.play();
+    var note1 = new Howl({
+        src: ["../sounds/pianoNotes/note" + root + ".wav"],
+        volume: 0.33
+      });
 
-    Chords.pop();
+    var note2 = new Howl({
+        src: ["../sounds/pianoNotes/note" + (root + Chords[whichChord].intervals[0]) + ".wav"],
+        volume: 0.33
+    });
+
+    var note3 = new Howl({
+        src: ["../sounds/pianoNotes/note" + (root + Chords[whichChord].intervals[1]) + ".wav"],
+        volume: 0.33
+    });
+
+    var note4 = new Howl({
+        src: ["../sounds/pianoNotes/note" + (root + Chords[whichChord].intervals[2]) + ".wav"],
+        volume: 0.33
+    });
+      
+      note1.play();
+
+      setTimeout(function() {
+        note2.play();
+      }, 70)
+
+      setTimeout(function() {
+        note3.play();
+      }, 140);
+    
+      setTimeout(function() {
+        note4.play();
+      }, 210);
+
 }
 
 function ChordRecognition(){
     return (
         <div id="chordRecognition">
-            <img onClick={playSound} className="tempPlayButton" src="../css/play-button.svg" alt="playbutton"/>
+            <img onClick={playChord} className="tempPlayButton" src="../css/play-button.svg" alt="playbutton"/>
             <AnswersButtons />
         </div>
     );
